9ec4e2efb2e81561d05956bf20a976aa9c9f9fd6
Davner_Cavalcante <49840964+davcavalc@users.noreply.github.com>
2021-04-10 13:26:42 -0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import {Router, Route} from 'svero';
+	import Header from './Header.svelte';
+	import Index from './Header.svelte';
+	import About from './Header.svelte';
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	
+</style>
+
+<Header/>
+
+<div class="container">
+
+	<Router>
+		<Route path='*' component={Index} />
+		<Route path='/About' component={About} />
+		<Route path='*' component={Index} />
+		
+	</Router>
+
+</div>
